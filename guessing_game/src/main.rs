2b09ae68b7da24b7d50ec5d976797cfeea962ad2
RobertP2705 <136493854+RobertP2705@users.noreply.github.com>
2025-04-01 11:30:39 -0500

    dd

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,12 +2,8 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
-
     println!("Please Input your guess.");
-
     let mut guess = String::new();
-
     io::stdin().read_line(&mut guess).expect("Failed to read");
-
     println!("You guessed: {}", guess);
 }
